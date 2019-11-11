6756d8698fd9e054b956c89cf1ab60ab4920b3cf
David Lougheed <david.lougheed@gmail.com>
2019-11-11 18:26:21 -0500

    Use chord_lib version getter, fix chord_lib old version in requirements

--- setup.py
+++ setup.py
@@ -2,14 +2,12 @@
 
 import setuptools
 
-from chord_example_service import __version__
-
 with open("README.md", "r") as rf:
     long_description = rf.read()
 
 setuptools.setup(
     name="chord_example_service",
-    version=__version__,
+    version="0.1.0",
 
     python_requires=">=3.6",
     install_requires=["chord_lib @ git+https://github.com/c3g/chord_lib", "Flask==1.1.1"],
